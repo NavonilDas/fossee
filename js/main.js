d7ef390bb3ceb75cc243bb62938b18b75a89a436
NavonilDas <navneeladas@gmail.com>
2018-07-02 15:59:55 +0530

    meged the template and theme

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-var navbar_showing = false; // check if the nav bar showing
+(function ($) {
+    $(function () {
+        $.fn.alertMess = function(data){
+            alert(data);
+        }
+    });
+})(jQuery);
+
+var navbar_showing = false;
 function showNavBar() {
     if (!navbar_showing) {
         document.getElementById("sideNavBar").style.width = "75%"; // set the width to 250px
@@ -10,9 +18,80 @@ function showNavBar() {
         document.querySelector("#sideNavBar ul").style.visibility = "hidden";
         document.getElementById("sideNavBar").style.width = "0"; // set the width to 0
     }
-    navbar_showing = !navbar_showing; // change the toggle
+    navbar_showing = !navbar_showing;
 }
 function closeBar() {
     navbar_showing = true;
     showNavBar();
 }
+
+
+var slideIndex = 0;
+var flex_stop_anim = false;
+var text_index = 0;
+
+
+function carousel() {
+    if (!flex_stop_anim) {
+        var i;
+        var x = document.getElementsByClassName("flex");
+        for (i = 0; i < x.length; i++) {
+            x[i].style = "margin-right:-200px;margin-left:200px;visibility:hidden;opacity:0;";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) { slideIndex = 1 }
+        x[slideIndex - 1].style = "margin-right:0;margin-left:0;visibility:visible;opacity:1;";
+    }
+}
+function NextFlex() {
+    var i;
+    var x = document.getElementsByClassName("flex");
+    for (i = 0; i < x.length; i++) {
+        x[i].style = "margin-right:-200px;margin-left:200px;visibility:hidden;opacity:0;";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style = "margin-right:0;margin-left:0;visibility:visible;opacity:1;";
+}
+function changeActivities(el, n) {
+    var e;
+    document.getElementById('a').style = "display:none;";
+    document.getElementById('b').style = "display:none;";
+    document.getElementById('c').style = "display:none;";
+    document.getElementById('d').style = "display:none;";
+    document.getElementById('e').style = "display:none;";
+    var rev = document.querySelectorAll('.act-tabs li');
+    rev.forEach(function (val, ar) {
+        val.classList.remove('active');
+    });
+    el.classList.add('active');
+    if (n == 1) e = 'a';
+    if (n == 2) e = 'b';
+    if (n == 3) e = 'c';
+    if (n == 4) e = 'd';
+    if (n == 5) e = 'e';
+    document.getElementById(e).style = "display:block;";
+}
+function changeNews(n) {
+    if (n == 1) {
+        document.getElementsByClassName("newscontent")[0].style = "display:block";
+        document.getElementsByClassName("policycontent")[0].style = "display:none";
+    } else {
+        document.getElementsByClassName("newscontent")[0].style = "display:none";
+        document.getElementsByClassName("policycontent")[0].style = "display:block";
+    }
+}
+
+setInterval(carousel, 7000);
+setInterval(function () {
+var el =  document.querySelector('#sidediv p');
+var ar = [
+    "To increase the use of open source software in the curriculum and research",
+    "To promote open source software and hardware",
+    "To create documentation",
+    "To Develop new open source software where required"
+];
+el.innerText = ar[text_index]+'.';
+text_index++;
+if(text_index > 3) text_index = 0;
+}, 5000);
--- theme-settings.php
+++ theme-settings.php
@@ -1,15 +1,19 @@
 <?php
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InvokeCommand;
 
 function fossee_form_system_theme_settings_alter(&$form, &$form_state) {
     
+    // $items_per_page = \Drupal::config('fossee.settings')->get('fossee_logo');
+    // dpm($items_per_page);
     $form['front_page_blck'] = array(
-        '#type'         => 'details',
-        '#title'        => t('Front Page Cards'),
+        '#type' => 'details',
+        '#title' => t('Front Page Cards'),
         '#description'  => t('Select Wether to enable or disable Block.'),
         '#weight' => -1000,
         '#open' => TRUE,
-      );
+    );
 
     $form['front_page_blck']['scilab_blck'] = array(
         '#type' => 'radios',
@@ -127,7 +131,57 @@ function fossee_form_system_theme_settings_alter(&$form, &$form_state) {
           0 => t('Disabled'),
           1 => t('Enabled')
         ),
-    ); 
+    );
+    $form['front_page_imgs'] = array(
+        '#type'=> 'details',
+        '#title'=> t('Front Page Images'),
+        '#description'=> t('Select the images.'),
+        '#weight' => -1000,
+        '#open' => TRUE,
+    );
+    $form['front_page_imgs']['img_one'] = array(
+        '#type'=>'file',
+        '#title'=>'First Animation Image',
+    );
+    $form['front_page_imgs']['img_one_ena'] = array(
+        '#type' => 'select',
+        '#title' => t('Selected'),
+        '#options' => array(
+           0 => t('Disable'),
+          1 => t('Enable'),
+        ),
+        '#default_value' => 1,
+        '#description' => t('Set this to <em>Yes</em> if you would like this category to be selected by default.'),
+    );
+    $form['front_page_imgs']['img_two'] = array(
+        '#type'=>'file',
+        '#title'=>'Second Animation Image',
+    );
+    $form['front_page_imgs']['img_three'] = array(
+        '#type'=>'file',
+        '#title'=>'Third Animation Image',
+    );
+    $form['front_page_imgs']['img_four'] = array(
+        '#type'=>'file',
+        '#title'=>'Fourth Animation Image',
+    );
+
+    $form['fossee_logo'] = array(
+        '#type'=> 'details',
+        '#title'=> t('Logo'),
+        '#description'=> t('Select the logo.'),
+        '#weight' => -1000,
+        '#open' => TRUE,
+    );
+    $form['fossee_logo']['the_logo'] = array(
+        '#type'=>'file',
+
+        // '#name'=>'the_logo',
+        '#title'=>'Upload Logo img',
+        // '#upload_location'=>'public://uploads'
+    );
+
+
     return $form;
 }
 
@@ -138,9 +192,4 @@ if(!function_exists('fossee_theme_get_settings')){
             return $default;
         return $val;
     }
-}
-
-
-function fossee_demo_ajax(){
-    dpm("snjjjs");
 }
