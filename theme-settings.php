690d49d92214b979045a36697a98248f375b2c9b
NavonilDas <navneeladas@gmail.com>
2018-07-06 10:51:50 +0530

    Display all uploaded animation images in settings form

--- theme-settings.php
+++ theme-settings.php
@@ -4,9 +4,7 @@ use Drupal\Core\Form\FormStateInterface;
 
 function fossee_form_system_theme_settings_alter(&$form, &$form_state)
 {
-
-    // dpm( \Drupal::config('fossee.settings')->get('themessage'));
-
+    $config = \Drupal::config('fossee.settings');
     $form['front_page_blck'] = array(
         '#type' => 'details',
         '#title' => t('Extra Cards'),
@@ -284,6 +282,30 @@ function fossee_form_system_theme_settings_alter(&$form, &$form_state)
     ];
 
     $form['anim_imgs']['thecontainer']['table'] = array('#markup' => '<table>');
+    $all_imgs = $config->get('all_imgs');
+    for ($i = 0; $i < count($all_imgs); $i++) {
+        if ($form_state->get('up_anim_img_' . $i) !== false) {
+            $form['anim_imgs']['thecontainer']['up_an_img_' . $i] = array(
+                '#type' => 'markup',
+                '#prefix' => '<tr><td>',
+                '#markup' => '' . $all_imgs[$i],
+                '#suffix' => '</td>',
+            );
+            $form['anim_imgs']['thecontainer']['up_img_del_' . $i] = array(
+                '#type' => 'submit',
+                '#name' => 'up_img_delete_'.$i,
+                '#value' => 'Delete',
+                '#prefix' => '<td>',
+                '#suffix' => '</td></tr>',
+                '#submit' => array('UPimageDelete'),
+                '#ajax' => array(
+                    'callback' => 'addImgCallback',
+                    'wrapper' => 'thecontainer--wrapper',
+                ),
+            );
+        }
+    }
+
     for ($i = 0; $i < $no_imgs; $i++) {
         $val = $form_state->get("uploaded_anim_img_" . $i);
         if ($form_state->get('anim_img_' . $i) !== false) {
@@ -332,7 +354,6 @@ function fossee_form_system_theme_settings_alter(&$form, &$form_state)
         '#type' => 'file',
         '#title' => 'Upload Logo img',
     );
-
     $form['#submit'][] = 'fossee_form_submit';
     return $form;
 }
@@ -344,6 +365,21 @@ function imageDelete(array &$form, FormStateInterface $form_state)
         $ind = (int) str_replace("img_delete_", "", $tg['#name']);
         $form_state->set("anim_img_" . $ind, false);
     }
+    $form_state->setRebuild();
+}
+function UPimageDelete(array &$form, FormStateInterface $form_state)
+{
+    $tg = $form_state->getTriggeringElement();
+    $config = \Drupal::configFactory()->getEditable('fossee.settings');
+    $connection = \Drupal::database();
+    $all_imgs = $config->get('all_imgs');
+    $ind = (int) str_replace("up_img_delete_", "", $tg['#name']);
+    $fid = $connection->query("DELETE FROM {file_managed} WHERE uri = :path", array(':path' => $all_imgs[$ind]));
+    $ret = unlink(str_replace('public://', 'sites/default/files/', $all_imgs[$ind]));
+    array_splice($all_imgs, $ind, 1);
+    $form_state->set("up_anim_img_" . $ind, false);
+    $config->set("all_imgs", $all_imgs);
+    $config->save();
     $form_state->setRebuild();
 }
 
@@ -389,6 +425,7 @@ function fossee_form_submit(array &$form, FormStateInterface $form_state)
     );
     $connection = \Drupal::database();
     $config = \Drupal::configFactory()->getEditable('fossee.settings');
+    $imgs = $config->get("all_imgs");
     if ($imgFile = file_save_upload('the_logo', $imgValidator, "public://", FILE_EXISTS_RENAME)) {
         $config->set("the_logo", $imgFile->destination);
     }
@@ -396,19 +433,20 @@ function fossee_form_submit(array &$form, FormStateInterface $form_state)
     if ($imgFile = file_save_upload((string) $key, $imgValidator, "public://", FILE_EXISTS_RENAME)) {
         $form_state->set('uploaded_' . $key, $imgFile->destination);
     }
-    $k = 0;
+    $k = count($imgs);
     for ($i = 0; $i < $all; $i++) {
         if ($form_state->get('anim_img_' . $i) !== false) {
             $val = $form_state->get("uploaded_anim_img_" . $i);
-            $key = 'anim_img_' . $i;
-            $config->set((string) $key, $val);
-            $k++;
+            $key = 'anim_img_' . $k;
+            if (!empty($val)) {
+                $imgs[] = $val;
+            }
         } else {
             $val = $form_state->get("uploaded_anim_img_" . $i);
             $fid = $connection->query("DELETE FROM {file_managed} WHERE uri = :path", array(':path' => $val));
             $ret = unlink(str_replace('public://', 'sites/default/files/', $val));
         }
     }
-    $config->set('anim_all', $k);
+    $config->set('all_imgs', $imgs);
     $config->save();
 }
